401bdc28d313cf27cc5e87a2a024e6b6bbb84c36
Taorem Lucky singh <67669132+LuckyTaorem@users.noreply.github.com>
2024-07-23 01:47:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,6 +14,48 @@ app.use(cors({
 mongoose.connect(`mongodb+srv://luckytaorem5:Lucky@luckycluster.son3r0i.mongodb.net/internships`).then(()=>{
     console.log("connected to database")
 })
+
+app.use("/test",()=>{
+    const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://luckytaorem5:Lucky@luckycluster.son3r0i.mongodb.net/fsmernlpu").then(()=>{
+    console.log("Connected to MongoDb");
+})
+
+const student = new mongoose.Schema({
+    name: String,
+    age:Number,
+    email:String,
+    address:String,
+    phone:Number
+})
+const Students = new mongoose.model("Students",student);
+//model is a collections inside mongoDb
+const adder =async ()=>{
+    const student1 = new Students({
+        name:"Rahul",
+        age:20,
+        email:"rahul@gmail.com",
+        address:"Bangalore",
+        phone:1234567890
+    })
+    await student1.save();
+    
+    // await Students.create({
+    //     name:"Rahul3",
+    //     age:19,
+    //     email:"rahul@gmail.com",
+    //     address:"Bangalore",
+    //     phone:1234567890
+    // })
+
+    const a1 = await Students.findOne({age:{$lt: 20}})
+    console.log(a1);
+
+}
+adder()
+})
+
 app.use("/auth",router)
 app.listen(4000,()=>{
     console.log("server is running on port 4000")
