1d2bf1dd110cc6ac7f012f6e429b3f3ecff53e60
Taorem Lucky singh <67669132+LuckyTaorem@users.noreply.github.com>
2024-07-23 01:58:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,60 +1,23 @@
-const express =require("express")
-const app = express()
-const cors = require("cors")
-const bodyParser = require("body-parser")
-const mongoose = require("mongoose")
-const router = require("./routes/userr")
-const cookieParser = require("cookie-parser")
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(cors({
-    origin:["https://mern-internship.onrender.com"],
-    credentials:true
-}))
-//mongoose.connect(`mongodb+srv://luckytaorem5:Lucky@luckycluster.son3r0i.mongodb.net/internships`).then(()=>{
-//    console.log("connected to database")
-//})
-mongoose.connect("mongodb+srv://luckytaorem5:Lucky@luckycluster.son3r0i.mongodb.net/fsmernlpu").then(()=>{
-    console.log("Connected to MongoDb");
-})
+const { MongoClient } = require("mongodb");
 
-app.use("/test",()=>{
-//    const mongoose = require('mongoose');
-const student = new mongoose.Schema({
-    name: String,
-    age:Number,
-    email:String,
-    address:String,
-    phone:Number
-})
-const Students = new mongoose.model("Students",student);
-//model is a collections inside mongoDb
-const adder =async ()=>{
-    const student1 = new Students({
-        name:"Rahul",
-        age:20,
-        email:"rahul@gmail.com",
-        address:"Bangalore",
-        phone:1234567890
-    })
-    await student1.save();
-    
-    // await Students.create({
-    //     name:"Rahul3",
-    //     age:19,
-    //     email:"rahul@gmail.com",
-    //     address:"Bangalore",
-    //     phone:1234567890
-    // })
+// Replace the uri string with your connection string.
+const uri = "mongodb+srv://luckytaorem5:<password>@luckycluster.son3r0i.mongodb.net/?retryWrites=true&w=majority&appName=LuckyCluster";
 
-    const a1 = await Students.findOne({age:{$lt: 20}})
-    console.log(a1);
+const client = new MongoClient(uri);
 
+async function run() {
+  try {
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
-adder()
-})
-
-app.use("/auth",router)
-app.listen(4000,()=>{
-    console.log("server is running on port 4000")
-})
+run().catch(console.dir);
